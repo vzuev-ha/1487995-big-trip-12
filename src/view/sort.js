2c4f8d588a4544605f03054f06b647d539579211
vzuev-ha <65515070+vzuev-ha@users.noreply.github.com>
2020-08-24 18:38:28 +0300

    Связываем сортировку и рендеринг (убрали бэкап разметки сортировки)

--- src/view/sort.js
+++ src/view/sort.js
@@ -76,30 +76,3 @@ export default class SortView extends AbstractView {
     this.getElement().addEventListener(`click`, this._sortTypeChangeHandler);
   }
 }
-
-
-// `            <div class="trip-sort__item  trip-sort__item--event">
-//               <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" checked>
-//               <label class="trip-sort__btn" for="sort-event" data-sort-type="${SortType.EVENT}">Event</label>
-//             </div>
-//
-//             <div class="trip-sort__item  trip-sort__item--time">
-//               <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
-//               <label class="trip-sort__btn" for="sort-time" data-sort-type="${SortType.TIME}">
-//                 Time
-//                 <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
-//                   <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
-//                 </svg>
-//               </label>
-//             </div>
-//
-//             <div class="trip-sort__item  trip-sort__item--price">
-//               <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
-//               <label class="trip-sort__btn" for="sort-price" data-sort-type="${SortType.PRICE}">
-//                 Price
-//                 <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
-//                   <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
-//                 </svg>
-//               </label>
-//             </div>
-// `
