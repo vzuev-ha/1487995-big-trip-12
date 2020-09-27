cf88f0c16840a5412b767549ddf85717cec4f4a4
vzuev-ha <65515070+vzuev-ha@users.noreply.github.com>
2020-09-28 00:56:46 +0300

    Устанавливаем пакеты для графиков и добавляем код коллеги

--- src/view/statistics.js
+++ src/view/statistics.js
@@ -1,4 +1,214 @@
 import SmartView from "./smart.js";
+import Chart from "chart.js";
+import ChartDataLabels from "chartjs-plugin-datalabels";
+
+// Рассчитаем высоту канваса в зависимости от того, сколько данных в него будет передаваться
+const BAR_HEIGHT = 55;
+
+
+const renderMoneyChart = (moneyCtx) => {
+  return new Chart(moneyCtx, {
+    plugins: [ChartDataLabels],
+    type: `horizontalBar`,
+    data: {
+      labels: [`FLY`, `STAY`, `DRIVE`, `LOOK`, `EAT`, `RIDE`],
+      datasets: [{
+        data: [400, 300, 200, 160, 150, 100],
+        backgroundColor: `#ffffff`,
+        hoverBackgroundColor: `#ffffff`,
+        anchor: `start`
+      }]
+    },
+    options: {
+      plugins: {
+        datalabels: {
+          font: {
+            size: 13
+          },
+          color: `#000000`,
+          anchor: `end`,
+          align: `start`,
+          formatter: (val) => `€ ${val}`
+        }
+      },
+      title: {
+        display: true,
+        text: `MONEY`,
+        fontColor: `#000000`,
+        fontSize: 23,
+        position: `left`
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            fontColor: `#000000`,
+            padding: 5,
+            fontSize: 13,
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+          barThickness: 44,
+        }],
+        xAxes: [{
+          ticks: {
+            display: false,
+            beginAtZero: true,
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+          minBarLength: 50
+        }],
+      },
+      legend: {
+        display: false
+      },
+      tooltips: {
+        enabled: false,
+      }
+    }
+  });
+};
+
+
+const renderTransportChart = (transportCtx) => {
+  return new Chart(transportCtx, {
+    plugins: [ChartDataLabels],
+    type: `horizontalBar`,
+    data: {
+      labels: [`DRIVE`, `RIDE`, `FLY`, `SAIL`],
+      datasets: [{
+        data: [4, 3, 2, 1],
+        backgroundColor: `#ffffff`,
+        hoverBackgroundColor: `#ffffff`,
+        anchor: `start`
+      }]
+    },
+    options: {
+      plugins: {
+        datalabels: {
+          font: {
+            size: 13
+          },
+          color: `#000000`,
+          anchor: `end`,
+          align: `start`,
+          formatter: (val) => `${val}x`
+        }
+      },
+      title: {
+        display: true,
+        text: `TRANSPORT`,
+        fontColor: `#000000`,
+        fontSize: 23,
+        position: `left`
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            fontColor: `#000000`,
+            padding: 5,
+            fontSize: 13,
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+          barThickness: 44,
+        }],
+        xAxes: [{
+          ticks: {
+            display: false,
+            beginAtZero: true,
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+          minBarLength: 50
+        }],
+      },
+      legend: {
+        display: false
+      },
+      tooltips: {
+        enabled: false,
+      }
+    }
+  });
+};
+
+
+const renderTimeSpendChart = (timeSpendCtx) => {
+  return new Chart(timeSpendCtx, {
+    plugins: [ChartDataLabels],
+    type: `horizontalBar`,
+    data: {
+      labels: [`HOTEL`, `RIDE`, `FLY`, `SAIL`],
+      datasets: [{
+        data: [4, 3, 2, 1],
+        backgroundColor: `#ffffff`,
+        hoverBackgroundColor: `#ffffff`,
+        anchor: `start`
+      }]
+    },
+    options: {
+      plugins: {
+        datalabels: {
+          font: {
+            size: 13
+          },
+          color: `#000000`,
+          anchor: `end`,
+          align: `start`,
+          formatter: (val) => `${val}H`
+        }
+      },
+      title: {
+        display: true,
+        text: `TRANSPORT`,
+        fontColor: `#000000`,
+        fontSize: 23,
+        position: `left`
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            fontColor: `#000000`,
+            padding: 5,
+            fontSize: 13,
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+          barThickness: 44,
+        }],
+        xAxes: [{
+          ticks: {
+            display: false,
+            beginAtZero: true,
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+          minBarLength: 50
+        }],
+      },
+      legend: {
+        display: false
+      },
+      tooltips: {
+        enabled: false,
+      }
+    }
+  });
+};
+
 
 const createStatisticsTemplate = () => {
   return `        <section class="statistics">
@@ -24,11 +234,19 @@ export default class Statistics extends SmartView {
 
     this._data = tripEvents;
 
+    this._moneyChart = null;
+    this._transportChart = null;
+    this._timeSpendChart = null;
+
     this._setCharts();
   }
 
   removeElement() {
     super.removeElement();
+
+    this._moneyChart = null;
+    this._transportChart = null;
+    this._timeSpendChart = null;
   }
 
   getTemplate() {
@@ -40,6 +258,21 @@ export default class Statistics extends SmartView {
   }
 
   _setCharts() {
+    this._moneyChart = null;
+    this._transportChart = null;
+    this._timeSpendChart = null;
 
+    const moneyCtx = this.getElement().querySelector(`.statistics__chart--money`);
+    moneyCtx.height = BAR_HEIGHT * 6;
+
+    const transportCtx = this.getElement().querySelector(`.statistics__chart--transport`);
+    transportCtx.height = BAR_HEIGHT * 4;
+
+    const timeSpendCtx = this.getElement().querySelector(`.statistics__chart--time`);
+    timeSpendCtx.height = BAR_HEIGHT * 4;
+
+    this._moneyChart = renderMoneyChart(moneyCtx);
+    this._transportChart = renderTransportChart(transportCtx);
+    this._timeSpendChart = renderTimeSpendChart(timeSpendCtx);
   }
 }
